34023fc080dc0dbb00fdcc06bac40013e7ab183d
jaitrinity <45747988+jaitrinity@users.noreply.github.com>
2024-05-15 18:14:44 +0530

    Delete cpDbConfig_db.php

--- cpDbConfig_db.php
+++
@@ -1,9 +0,0 @@
-<?php
-$conn=mysqli_connect("localhost","db","P@ssw0rd","TVICustomerPortal");
-// $conn=mysqli_connect("localhost","root","Tr!n!ty@pp1@b","TVICustomerPortal");
-if (mysqli_connect_errno()){
-	echo "Failed to connect to MySQL: " . mysqli_connect_error();
-	exit();
-}
-mysqli_set_charset($conn, 'utf8');
-?>
